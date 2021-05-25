d242c0fecd4f7ce28dc1cb83cd140341e1121c05
Rafael Santos <rf.sants001@gmail.com>
2021-05-24 23:27:18 -0300

    Finish Home

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // function([string1, string2],target id,[color1,color2])    
 consoleText([' Design.', ' Express.'], 'text',['#9A7CDD','#9A7CDD']);
 
+
+
 function consoleText(words, id, colors) {
+  var subscriptionText = document.querySelector('#text-subscribe');
   if (colors === undefined) colors = ['#fff'];
   var visible = true;
   var con = document.getElementById('console');
@@ -47,5 +50,48 @@ function consoleText(words, id, colors) {
 
       visible = true;
     }
-  }, 400)
+  }, 400)  
 }
+
+
+/*Typing effect */
+
+const words = ["Lets subscribe in our list."];
+let i = 0;
+let timer;
+
+function typingEffect() {
+	let word = words[i].split("");
+	var loopTyping = function() {
+		if (word.length > 0) {
+			document.getElementById('word').innerHTML += word.shift();
+		} else {
+			deletingEffect();
+			return false;
+		};
+		timer = setTimeout(loopTyping, 500);
+	};
+	loopTyping();
+};
+
+function deletingEffect() {
+	let word = words[i].split("");
+	var loopDeleting = function() {
+		if (word.length > 0) {
+			word.pop();
+			document.getElementById('word').innerHTML = word.join("");
+		} else {
+			if (words.length > (i + 1)) {
+				i++;
+			} else {
+				i = 0;
+			};
+			typingEffect();
+			return false;
+		};
+		timer = setTimeout(loopDeleting, 200);
+	};
+	loopDeleting();
+};
+
+typingEffect();
